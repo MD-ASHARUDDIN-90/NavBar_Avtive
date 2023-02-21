75dcc348cec38781e5d2d8909fca514f65a47b8b
MD-ASHARUDDIN-90 <119596353+MD-ASHARUDDIN-90@users.noreply.github.com>
2023-02-21 12:12:32 +0530

    4 files modified, 3 added actual use of navlink with classname

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './style.css';
+import NavBar from './NavBar'
+export default function About() {
+ 
+
+  return (
+    <>
+      <NavBar />
+    <div className="HomePage">
+    <h1>ABOUT PAGE</h1>
+    </div>
+    </>
+  );
+}
---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './style.css';
+import NavBar from './NavBar'
+export default function Contact() {
+ 
+
+  return (
+    <>
+      <NavBar />
+    <div className="HomePage">
+    <h1>CONTACT PAGE</h1>
+    </div>
+    </>
+  );
+}
